e01be38d8f6f21d00e7f6fc887ac6c313dbc0efd
Aisha <kalimatovaaisha@mail.ru>
2023-05-22 12:34:44 +0600

    add testing in Main class

--- Main.java
+++ Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        BST<Integer, String> bst = new BST<>();
+        int i = 0;
+        while (i <= 15) {
+            bst.put(i, "" + i);
+            i++;
+        }
+        Iterable iterator = bst.iterator();
+        for (Object elem : iterator) {
+            System.out.println(elem);
+        }
+    }
 }
