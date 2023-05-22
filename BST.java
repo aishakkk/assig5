1dd3c05c714c4e0976e6a7b23ad9376de6859fab
Aisha <kalimatovaaisha@mail.ru>
2023-05-22 12:23:05 +0600

    supplement put method with additional insertNode method

--- BST.java
+++ BST.java
@@ -11,9 +11,30 @@ public class BST<K extends Comparable<K>, V> {
             this.key = key;
             this.val = val;
         }
+        @Override
+        public String toString() {
+            return "{key is " + this.key + " and value is " + this.val + "}";
+        }
     }
-    public void put(K key, V val) {
-
+    public int getSize() {
+        return size;
+    }
+    public void put(K key, V value) {
+        this.root = insertNode(root, key, value);
+        size++;
+    }
+    private Node insertNode(Node node, K key, V val) {
+        if (node == null) {
+            return new Node(key, val);
+        }
+        if (node.key.compareTo(key) == 1) {
+            node.left = insertNode(node.left, key, val);
+        } else if(node.key.compareTo(key) == -1) {
+            node.right = insertNode(node.right, key, val);
+        } else {
+            node.val = val;
+        }
+        return node;
     }
     public V get(K key) {
         return null;
